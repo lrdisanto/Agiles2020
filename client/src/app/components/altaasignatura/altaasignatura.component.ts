97e5c52e9acc95372a07f86ed1becfb6ba73fba9
antobasalo <antonella.basalo@gmail.com>
2020-11-20 22:46:42 -0300

    Agregue un combo para que se puedan seleccionar los docentes cuando se carga una materia

--- client/src/app/components/altaasignatura/altaasignatura.component.ts
+++ client/src/app/components/altaasignatura/altaasignatura.component.ts
@@ -1,28 +1,43 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { Materia } from 'src/app/models/Materia';
+import { Profesor } from 'src/app/models/profesor';
+
 
 import { MateriaService } from '../../services/materia.service';
+import {ProfesoresService} from '../../services/profesores.service';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-altaasignaturas',
   templateUrl: './altaasignatura.component.html',
   styleUrls: ['./altaasignatura.component.css']
 })
+
 export class AltaasignaturaComponent implements OnInit {
 
 //  @HostBinding('class') class='row'; // ---> esto hace que el formulario sea una unica row en toda la pantalla, lo dejo por las dudas
-  
+  profesor: any=[];
   materia: Materia = {
     idanio: 1,
     idprofesor:1, 
     nombre: '',
    };
 
-  constructor(private materiaService: MateriaService, private router: Router) { }
+  constructor(private materiaService: MateriaService, private router: Router, private profesoresService: ProfesoresService) { }
 
   ngOnInit(): void {
-  }
+  
+  this.profesoresService.getProfesores().subscribe(
+    res => {
+      this.profesor = res;
+        
+    },
+    err=> {console.error(err);
+     // this.contenidos=[];
+    
+      }
+    ) }
 
   saveNewMateria(){
 
